702a1fcdcd15b621d0cccb503dc846e13302ceef
David Matz <XM9J59@gmail.com>
2026-05-22 08:07:28 -0400

    0.7.0

---
+++ dev/git_push_all.sh
@@ -0,0 +1,73 @@
+git add .
+git commit -m "0.7.0"
+git push
+
+cd ../fhir_r4b
+git add .
+git commit -m "0.7.0"
+git push
+
+cd ../fhir_r4us
+git add .
+git commit -m "0.7.0"
+git push
+
+cd ../fhir_r4p
+git add .
+git commit -m "0.7.0"
+git push
+
+cd ../fhir_r5
+git add .
+git commit -m "0.7.0"
+git push
+
+cd ../fhir_client_httpc
+git add .
+git commit -m "0.7.0"
+git push
+
+cd ../fhir_client_rsvp
+git add .
+git commit -m "0.7.0"
+git push
+
+cd ../fhir_r4b_client_httpc
+git add .
+git commit -m "0.7.0"
+git push
+
+cd ../fhir_r4b_client_rsvp
+git add .
+git commit -m "0.7.0"
+git push
+
+cd ../fhir_r4p_client_httpc
+git add .
+git commit -m "0.7.0"
+git push
+
+cd ../fhir_r4p_client_rsvp
+git add .
+git commit -m "0.7.0"
+git push
+
+cd ../fhir_r4us_client_httpc
+git add .
+git commit -m "0.7.0"
+git push
+
+cd ../fhir_r4us_client_rsvp
+git add .
+git commit -m "0.7.0"
+git push
+
+cd ../fhir_r5_client_httpc
+git add .
+git commit -m "0.7.0"
+git push
+
+cd ../fhir_r5_client_rsvp
+git add .
+git commit -m "0.7.0"
+git push
